a7366fbcd7ee7a6da7dadd03bad277d6701c8529
Preetam Raj <iitianpreetam@gmail.com>
2021-05-29 15:11:18 +0530

    initial commit

---
+++ src/SearchForm.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+const SearchForm = ({ searchText }) => {
+    const [text, setText] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        searchText(text);
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text"
+                    placeholder="e.g. politics"
+                    className="py-1 px-2 rounded-l-lg"
+                    onChange={(e) => setText(e.target.value)}
+                />
+                <button type="submit" className="bg-green-400 py-1 px-2 rounded-r-lg text-white">Search</button>
+            </form>
+        </div>
+    );
+}
+
+export default SearchForm;
